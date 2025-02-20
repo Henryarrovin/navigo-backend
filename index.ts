12b9a8a1b6388fd09b244ecac8056869a4f0dc15
Henryarrovin <henry.a.s.arrovin@gmail.com>
2025-02-20 21:08:15 +0530

    Connected the application to mongoDB ... fixes #1 and created the project structure.

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
-import type { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import productRoutes from './routes/productRoutes';
+
+dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = 8080;
 
-app.get('/hello', (req: Request, res: Response) => {
-  res.send("hello!");
-});
+mongoose
+    .connect(process.env.MONGO_URI as string)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.log("Error connecting to MongoDB:", error.message));
+
+app.use("/api", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
